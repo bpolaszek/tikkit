d05ca20078490e2ef31ee0265d9ce630c49026b2
Beno!t POLASZEK <bpolaszek@gmail.com>
2025-05-23 11:33:00 +0200

    update nuxt.config.ts to set dev server port and rename application to tikkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  devServer: {
+    port: 3007,
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
